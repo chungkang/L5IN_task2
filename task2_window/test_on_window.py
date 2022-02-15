1afe0ad541d726bfbf18c293c996346292bd1369
unknown <chungkang.c@gmail.com>
2022-02-15 11:11:59 +0100

    rectify

--- task2_window/test_on_window.py
+++ task2_window/test_on_window.py
@@ -9,18 +9,20 @@ import PIL
 from datetime import datetime
 import cv2
 
+import module.rectify as rectify
+
 current_path = os.path.dirname(os.path.realpath(__file__)) #파일 경로
 
 # Settings
 root = Tk()
 root.title("Map Generator from Rescue Plans")
-root.geometry("800x600")
+root.geometry("800x800")
 # root.resizable(False, False)
 title = Label(root, text="Map Generator")
 title.pack()
 
 # notebook
-notebook = tkinter.ttk.Notebook(root, width=700, height=500, takefocus=True)
+notebook = tkinter.ttk.Notebook(root, width=700, height=700, takefocus=True)
 notebook.pack(fill=tkinter.BOTH)
 
 rootHeight = root.winfo_height()
@@ -36,6 +38,8 @@ rectify_yn = 0	# image rectify done yn
 frame1_count = 0	# frame1 process count
 new_dir_nm = current_path	# using directory name
 frame1_save_image = ''	# frame1's image path
+frame2_image_points = ''
+frame2_control_points = ''
 
 def file_find():
 	image_ext = r"*.jpg *.jpeg *.png"
@@ -118,47 +122,58 @@ notebook.add(frame2, text="2: Rectification")
 image_point_title = Label(frame2, width=20, text="Image Points: ")
 image_point_title.grid(row=0, column=0, columnspan=2)
 
-image_point_nw_nm = Label(frame2, width=20, text="nw: ")
-image_point_nw_nm.grid(row=1, column=0)
-image_point_nw = Entry(frame2, width=20)
-image_point_nw.grid(row=1, column=1)
+image_point_1_nm = Label(frame2, width=20, text="1: ")
+image_point_1_nm.grid(row=1, column=0)
+image_point_1 = Entry(frame2, width=20)
+image_point_1.insert(0, "427;1013")
+image_point_1.grid(row=1, column=1)
 
-image_point_ne_nm = Label(frame2, width=20, text="ne: ")
-image_point_ne_nm.grid(row=2, column=0)
-image_point_ne = Entry(frame2, width=20)
-image_point_ne.grid(row=2, column=1)
+image_point_2_nm = Label(frame2, width=20, text="2: ")
+image_point_2_nm.grid(row=2, column=0)
+image_point_2 = Entry(frame2, width=20)
+image_point_2.insert(0, "403;1580")
+image_point_2.grid(row=2, column=1)
 
-image_point_sw_nm = Label(frame2, width=20, text="sw: ")
-image_point_sw_nm.grid(row=3, column=0)
-image_point_sw = Entry(frame2, width=20)
-image_point_sw.grid(row=3, column=1)
+image_point_3_nm = Label(frame2, width=20, text="3: ")
+image_point_3_nm.grid(row=3, column=0)
+image_point_3 = Entry(frame2, width=20)
+image_point_3.insert(0, "3810;1223")
+image_point_3.grid(row=3, column=1)
 
-image_point_se_nm = Label(frame2, width=20, text="se: ")
-image_point_se_nm.grid(row=4, column=0)
-image_point_se = Entry(frame2, width=20)
-image_point_se.grid(row=4, column=1)
+image_point_4_nm = Label(frame2, width=20, text="4: ")
+image_point_4_nm.grid(row=4, column=0)
+image_point_4 = Entry(frame2, width=20)
+image_point_4.insert(0, "3833;1590")
+image_point_4.grid(row=4, column=1)
 
 control_point_title = Label(frame2, width=20, text="Control Points: ")
 control_point_title.grid(row=0, column=3, columnspan=2)
-control_point_nw_nm = Label(frame2, width=20, text="nw: ")
-control_point_nw_nm.grid(row=1, column=2)
-control_point_nw = Entry(frame2, width=20)
-control_point_nw.grid(row=1, column=3)
+control_point_1_nm = Label(frame2, width=20, text="1: ")
+control_point_1_nm.grid(row=1, column=2)
+control_point_1 = Entry(frame2, width=20)
+control_point_1.insert(0, "0;0")
+control_point_1.grid(row=1, column=3)
 
-control_point_ne_nm = Label(frame2, width=20, text="ne: ")
-control_point_ne_nm.grid(row=2, column=2)
-control_point_ne = Entry(frame2, width=20)
-control_point_ne.grid(row=2, column=3)
+control_point_2_nm = Label(frame2, width=20, text="2: ")
+control_point_2_nm.grid(row=2, column=2)
+control_point_2 = Entry(frame2, width=20)
+control_point_2.insert(0, "0;880")
+control_point_2.grid(row=2, column=3)
 
-control_point_sw_nm = Label(frame2, width=20, text="sw: ")
-control_point_sw_nm.grid(row=3, column=2)
-control_point_sw = Entry(frame2, width=20)
-control_point_sw.grid(row=3, column=3)
+control_point_3_nm = Label(frame2, width=20, text="3: ")
+control_point_3_nm.grid(row=3, column=2)
+control_point_3 = Entry(frame2, width=20)
+control_point_3.insert(0, "4075;85")
+control_point_3.grid(row=3, column=3)
 
-control_point_se_nm = Label(frame2, width=20, text="se: ")
-control_point_se_nm.grid(row=4, column=2)
-control_point_se = Entry(frame2, width=20)
-control_point_se.grid(row=4, column=3)
+control_point_4_nm = Label(frame2, width=20, text="4: ")
+control_point_4_nm.grid(row=4, column=2)
+control_point_4 = Entry(frame2, width=20)
+control_point_4.insert(0, "4075;880")
+control_point_4.grid(row=4, column=3)
+
+control_description = Label(frame2, width=100, text="ex) 1000;200 (x;y)")
+control_description.grid(row=5, column=0, columnspan=4)
 
 fixed_width = 700
 frame2_image = Image.open("task2_window\\a.png")
@@ -167,10 +182,33 @@ image_height = int(float(frame2_image.size[1])*float(image_percent))
 frame2_image = frame2_image.resize((fixed_width, image_height), PIL.Image.NEAREST)
 frame2_photo = ImageTk.PhotoImage(frame2_image)
 frame2_image_label = Label(frame2, image=frame2_photo)
-frame2_image_label.grid(row=5, column=0, columnspan=4, sticky=W+E+N+S)
+frame2_image_label.grid(row=6, column=0, columnspan=4, sticky=W+E+N+S)
 
-bt_02_next = Button(frame2, text="Next", width=10, overrelief="solid", command= f_01_next)
-bt_02_next.grid(row=6, column=3)
+value1 = Entry(frame2, width=20)
+value1.insert(0, "1000")
+value1.grid(row=7, column=3)
+
+value2 = Entry(frame2, width=20)
+value2.insert(0, "-2500")
+value2.grid(row=7, column=4)
+
+def f_02_rectify():
+	global frame2_image_points
+	frame2_image_points = str(image_point_1.get()) + "\n" + str(image_point_2.get()) + "\n" + str(image_point_3.get()) + "\n" + str(image_point_4.get())
+	global frame2_control_points
+	frame2_control_points = str(control_point_1.get()) + "\n" + str(control_point_2.get()) + "\n" + str(control_point_3.get()) + "\n" + str(control_point_4.get())
+	
+	global new_dir_nm
+	e_image = rectify.fn_rectify(new_dir_nm,'01_original.png',frame2_image_points,frame2_control_points,value1.get(),value2.get())
+
+def f_02_next():
+	return ''
+
+bt_02_rectify = Button(frame2, text="Retify", width=10, overrelief="solid", command= f_02_rectify)
+bt_02_rectify.grid(row=7, column=0)
+
+bt_02_next = Button(frame2, text="Next", width=10, overrelief="solid", command= f_02_next)
+bt_02_next.grid(row=7, column=1)
 
 # frame. 3
 frame3 = Frame(root)
