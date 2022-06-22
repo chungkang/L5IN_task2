d264bbf91099a12e38eec6bad9fcdd965003a2be
unknown <chungkang.c@gmail.com>
2022-06-22 19:27:18 +0200

    image

--- task2_execute.py
+++ task2_execute.py
@@ -27,14 +27,18 @@ def image_find():
 	en_filepath.insert(tk.END, file[0])
 
 def popup_image():
-    win = tk.Toplevel()
-    win.wm_title("1. Image")
+    win_image = tk.Toplevel()
+    win_image.wm_title("1. Image")
 
-    l = tk.Label(win, text=en_filepath.get())
-    l.grid(row=0, column=0)
-
-    b = ttk.Button(win, text="Okay", command=win.destroy)
-    b.grid(row=1, column=0)
+    fixed_width = 600
+    image1 = PIL.Image.open(en_filepath.get())
+    image_percent = (fixed_width/float(image1.size[0]))
+    image_height = int(float(image1.size[1])*float(image_percent))
+    image1 = image1.resize((fixed_width, image_height), PIL.Image.NEAREST)
+    photo1 = PIL.ImageTk.PhotoImage(image1)
+    label1 = tk.Label(win_image, image=photo1)
+    label1.image = photo1
+    label1.grid(row=0, column=0)
 
 def popup_rectify():
     win = tk.Toplevel()
@@ -68,37 +72,37 @@ def filtering():
 
 # grid setting
 root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=4)
+root.columnconfigure(1, weight=1)
 root.columnconfigure(2, weight=1)
 # root.container.rowconfigure(0, 1)
 
 
 # 1. Select Image - popup image show
-tk.Label(root, text="1. Select Image: ").pack(side="top")
-en_filepath = tk.Entry(root, width=100)
-en_filepath.pack(side="top")
-tk.Button(root, text="Find", width=10, command=image_find).pack(side="top")
-tk.Button(root, text="Show Image", width=10, command=popup_image).pack(side="top")
+tk.Label(root, text="1. Select Image: ").grid(row=0,column=0)
+tk.Button(root, text="Show Image", width=10, command=popup_image).grid(row=0,column=2)
+en_filepath = tk.Entry(root)
+en_filepath.grid(row=1,column=0,columnspan=2)
+tk.Button(root, text="Find", width=10, command=image_find).grid(row=1,column=2)
 
 # 2. Rectify - popup image show
-tk.Label(root, text="2. Rectify").pack(side="top")
-tk.Button(root, text="Rectify", width=10, command=popup_rectify).pack(side="top")
+tk.Label(root, text="2. Rectify").grid(row=2,column=0)
+tk.Button(root, text="Rectify", width=10, command=popup_rectify).grid(row=2,column=1)
 
 # 3. Binarization - popup image show
-tk.Label(root, text="3. Binarization").pack(side="top")
-tk.Button(root, text="Binarization", width=10, command=popup_binarization).pack(side="top")
+tk.Label(root, text="3. Binarization").grid(row=3,column=0)
+tk.Button(root, text="Binarization", width=10, command=popup_binarization).grid(row=3,column=1)
 
 # 4. Neighbor - popup image show
-tk.Label(root, text="4. Neighbor").pack(side="top")
-tk.Button(root, text="Neighbor", width=10, command=neighbor).pack(side="top")
+tk.Label(root, text="4. Neighbor").grid(row=4,column=0)
+tk.Button(root, text="Neighbor", width=10, command=neighbor).grid(row=4,column=1)
 
 # 5. Vetorize - popup image show
-tk.Label(root, text="5. Vetorize").pack(side="top")
-tk.Button(root, text="Vetorize", width=10, command=vectorize).pack(side="top")
+tk.Label(root, text="5. Vetorize").grid(row=5,column=0)
+tk.Button(root, text="Vetorize", width=10, command=vectorize).grid(row=5,column=1)
 
 # 6. Filtering - popup image show
-tk.Label(root, text="6. Filtering").pack(side="top")
-tk.Button(root, text="Filtering", width=10, command=filtering).pack(side="top")
+tk.Label(root, text="6. Filtering").grid(row=6,column=0)
+tk.Button(root, text="Filtering", width=10, command=filtering).grid(row=6,column=1)
 
 # ttk.Button(root, text='Rock', command=lambda: select('Rock')).pack()
 
