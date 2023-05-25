05b7b6da72d6d375a51a645acd384799ad684d2a
unknown <chungkang.c@gmail.com>
2023-05-25 09:08:53 +0200

    feature matching

--- module_test/05_11_feature_matching_test.py
+++ module_test/05_11_feature_matching_test.py
@@ -19,12 +19,12 @@ def show_image(img, title, colorspace):
 
 
 # Function to match features and find the object
-def match_feature_find_object(query_img, train_img, min_matches): 
+def match_feature_find_object(template_img, background_img, min_matches): 
     # Create a SIFT object
     sift = cv2.SIFT_create()
 
-    features1, des1 = sift.detectAndCompute(query_img, None)
-    features2, des2 = sift.detectAndCompute(train_img, None)
+    features1, des1 = sift.detectAndCompute(template_img, None)
+    features2, des2 = sift.detectAndCompute(background_img, None)
 
     # Create Brute-Force matcher object
     bf = cv2.BFMatcher(cv2.NORM_L2)
@@ -35,7 +35,7 @@ def match_feature_find_object(query_img, train_img, min_matches):
     good_without_lists = []    
     matches = [match for match in matches if len(match) == 2] 
     for m, n in matches:
-        if m.distance < 0.8 * n.distance:
+        if m.distance < 0.7 * n.distance:
             good.append([m])
             good_without_lists.append(m)
             
@@ -48,35 +48,27 @@ def match_feature_find_object(query_img, train_img, min_matches):
         M, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
                 
         # Find the perspective transformation to get the corresponding points
-        h, w = query_img.shape[:2]
-        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+        h, w = template_img.shape[:2]
+        pts = np.float32([[-7, -7], [-7, h + 7], [w + 7, h + 7], [w + 7, -7]]).reshape(-1, 1, 2)
         dst = cv2.perspectiveTransform(pts, M)
+
+        # option 1 - only show green rectangle        
+        # background_img = cv2.polylines(background_img, [np.int32(dst)], True, (0, 255, 0), 2, cv2.LINE_AA)
         
-        train_img = cv2.polylines(train_img, [np.int32(dst)], True, (0, 255, 0), 2, cv2.LINE_AA)
-        
+        # optioon 2 - cover the detected feature with white color
         # Mask the detected feature with white color
-        mask = np.zeros_like(train_img, dtype=np.uint8)
+        mask = np.zeros_like(background_img, dtype=np.uint8)
         mask = cv2.fillPoly(mask, [np.int32(dst)], (255, 255, 255))
         
-        # Cover the detected feature with white color rectangle
-        train_img = cv2.rectangle(train_img, (dst[0][0], dst[0][1]), (dst[2][0], dst[2][1]), (255, 255, 255), -1)
-
-        # # Mask the detected feature with white color
-        # mask = np.zeros_like(train_img, dtype=np.uint8)
-        # mask = cv2.fillPoly(mask, [np.int32(dst)], (255, 255, 255))
-        
-        # # Combine the masked image with the original image
-        # masked_img = cv2.bitwise_and(train_img, mask)
-        
-        # # Cover the detected feature with white color
-        # train_img = cv2.addWeighted(masked_img, 1, train_img, 0, 0)
-
+        # Apply the mask to remove the symbol from the image
+        background_img = cv2.bitwise_or(background_img, mask)
 
     else:
         print('Not enough good matches are found - {}/{}'.format(len(good), min_matches))
             
-    result_img = cv2.drawMatchesKnn(query_img, features1, train_img, features2, good, None, flags = 2)
+    result_img = cv2.drawMatchesKnn(template_img, features1, background_img, features2, good, None, flags = 2)
     cv2.imwrite(file_name + "_detect.png", result_img)
+    cv2.imwrite(file_name + "_generated.png", background_img)
 
     # show_image(mask,'test','RGB')
 
@@ -87,8 +79,8 @@ def match_feature_find_object(query_img, train_img, min_matches):
 file_name = "module_test\\result\\IMG_3751_rect_crop_bilateral_crop"
 image_name = file_name + ".png"
 
-trainImage = cv2.imread(image_name) # trainImage
-queryImage = cv2.imread(file_name + '_template1.png') # queryImage
-# queryImage = cv2.imread( "module_test\\result\\image2-2_crop_bilateral_template1.png")
+backgroundImage = cv2.imread(image_name)
+templateImage = cv2.imread(file_name + '_template1.png')
+# templateImage = cv2.imread( "module_test\\result\\image2-2_crop_bilateral_template1.png")
 
-match_feature_find_object(queryImage, trainImage, 10)
+match_feature_find_object(templateImage, backgroundImage, 7)
