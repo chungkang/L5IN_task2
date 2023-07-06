be97af22ce261270db537cde34ccdf98c3ef322f
unknown <chungkang.c@gmail.com>
2023-07-06 12:37:24 +0200

    report

--- module_test/step04_image_stitcher.py
+++ module_test/step04_image_stitcher.py
@@ -4,7 +4,7 @@ import numpy as np
 import random
 from tqdm.notebook import tqdm
 
-# Read image and convert them to gray!!
+# Read image and convert them to gray
 def read_image(path):
     img = cv2.imread(path)
     img_gray= cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -17,11 +17,6 @@ def SIFT(img):
 
     kp, des = siftDetector.detectAndCompute(img, None)
     return kp, des
-
-def plot_sift(gray, rgb, kp):
-    tmp = rgb.copy()
-    img = cv2.drawKeypoints(gray, kp, tmp, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    return img
 
 def matcher(kp1, des1, img1, kp2, des2, img2, threshold):
     # BFMatcher with default params
@@ -40,22 +35,6 @@ def matcher(kp1, des1, img1, kp2, des2, img2, threshold):
 
     matches = np.array(matches)
     return matches
-
-
-def plot_matches(matches, total_img):
-    match_img = total_img.copy()
-    offset = total_img.shape[1]/2
-    fig, ax = plt.subplots()
-    ax.set_aspect('equal')
-    ax.imshow(np.array(match_img).astype('uint8')) #　RGB is integer type
-    
-    ax.plot(matches[:, 0], matches[:, 1], 'xr')
-    ax.plot(matches[:, 2] + offset, matches[:, 3], 'xr')
-     
-    ax.plot([matches[:, 0], matches[:, 2] + offset], [matches[:, 1], matches[:, 3]],
-            'r', linewidth=0.5)
-
-    plt.show()
 
 def homography(pairs):
     rows = []
@@ -181,9 +160,6 @@ right_gray, right_origin, right_rgb = read_image(file_path + '3OG_3_result.png')
 # Better result when using gray
 kp_left, des_left = SIFT(left_gray)
 kp_right, des_right = SIFT(right_gray)
-
-kp_left_img = plot_sift(left_gray, left_rgb, kp_left)
-kp_right_img = plot_sift(right_gray, right_rgb, kp_right)
 
 matches = matcher(kp_left, des_left, left_rgb, kp_right, des_right, right_rgb, 0.6)
 
